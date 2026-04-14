67b98639db2bed5000d2bdfde309f698d18e36a3
Federico <47462792+wtfzambo@users.noreply.github.com>
2026-04-14 02:44:56 +0200

    fix(mac): replace template Info.plist with concrete values
    Wails v3 Taskfile packaging bypasses template substitution, leaving literal Go template syntax in the macOS app bundle and breaking CFBundleIconFile.

--- scripts/set-release-version.sh
+++ scripts/set-release-version.sh
@@ -11,3 +11,7 @@ version=${1#v}
 
 perl -0pi -e 's/^  version: ".*" # The application version$/  version: "'"$version"'" # The application version/m' build/config.yml
 perl -0pi -e 's/^version: .*/version: '"$version"'/m' build/linux/nfpm/nfpm.yaml
+perl -0pi -e 's/<key>CFBundleVersion<\/key>\n\s*<string>.*<\/string>/<key>CFBundleVersion<\/key>\n        <string>'"$version"'<\/string>/m' build/darwin/Info.plist
+perl -0pi -e 's/<key>CFBundleShortVersionString<\/key>\n\s*<string>.*<\/string>/<key>CFBundleShortVersionString<\/key>\n        <string>'"$version"'<\/string>/m' build/darwin/Info.plist
+perl -0pi -e 's/<key>CFBundleVersion<\/key>\n\s*<string>.*<\/string>/<key>CFBundleVersion<\/key>\n        <string>'"$version"'<\/string>/m' build/darwin/Info.dev.plist
+perl -0pi -e 's/<key>CFBundleShortVersionString<\/key>\n\s*<string>.*<\/string>/<key>CFBundleShortVersionString<\/key>\n        <string>'"$version"'<\/string>/m' build/darwin/Info.dev.plist
